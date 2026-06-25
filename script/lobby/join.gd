5d5e39aa8a80d7bef4127398a795dc75fbe27352
moyashi170607 <96937973+moyashi170607@users.noreply.github.com>
2026-06-25 16:04:44 +0900

    feat:UIとシグナリングサーバーの機能を結びつけ

--- script/lobby/join.gd
+++ script/lobby/join.gd
@@ -1,15 +1,14 @@
 extends Control
 
-@onready var build_room_node:Control = $BuildRoom
+@onready var join_room_node:Control = $JoinRoom
 @onready var matching_node:Control = $Matching
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	build_room_node.visible = true
+	join_room_node.visible = true
 	matching_node.visible = false
-	
 
 
-func _on_build_room_success_build() -> void:
-	build_room_node.visible = false
+func _on_join_room_success_join() -> void:
+	join_room_node.visible = false
 	matching_node.visible = true
--- script/lobby/join_room.gd
+++ script/lobby/join_room.gd
@@ -1,13 +1,13 @@
 extends Control
 
-signal success_build()
+signal success_join()
 
 @onready var name_edit_node:LineEdit = $NameEdit
 @onready var room_edit_node:LineEdit = $RoomEdit
 @onready var button_node:Button = $Button
 
 func signal_disconnect() -> void:
-	NetworkManager.room_created.disconnect(_on_room_created)
+	NetworkManager.joined_room.disconnect(_on_joined_room)
 	NetworkManager.signaling_error.disconnect(_on_signaling_error)
 
 func _on_button_pressed() -> void:
@@ -15,13 +15,13 @@ func _on_button_pressed() -> void:
 	var roomname:String = room_edit_node.text
 	
 	if username.length() > 0 and roomname.length() > 0:
-		NetworkManager.host_game(username,roomname,2)
-		NetworkManager.room_created.connect(_on_room_created)
+		NetworkManager.join_game(username,roomname)
+		NetworkManager.joined_room.connect(_on_joined_room)
 		NetworkManager.signaling_error.connect(_on_signaling_error)
 
-func _on_room_created() -> void:
-	success_build.emit()
-	signal_disconnect()
+func _on_joined_room():
+	success_join.emit();
+	signal_disconnect();
 
-func _on_signaling_error() -> void:
-	signal_disconnect()
+func _on_signaling_error():
+	signal_disconnect();
