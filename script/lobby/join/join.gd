1ea1dba1ef63fa85949a8810966d76763f9babec
moyashi170607 <96937973+moyashi170607@users.noreply.github.com>
2026-06-26 18:40:18 +0900

    feat:テスト用シーンの準備

--- script/lobby/join/join.gd
+++ script/lobby/join/join.gd
@@ -1,7 +1,9 @@
 extends Control
 
+const Matching = preload("res://script/lobby/join/matching_join.gd")
+
 @onready var join_room_node:Control = $JoinRoom
-@onready var matching_node:Control = $Matching
+@onready var matching_node:Matching = $Matching
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,6 +11,8 @@ func _ready() -> void:
 	matching_node.visible = false
 
 
-func _on_join_room_success_join() -> void:
+func _on_join_room_success_join(hostname:String,password:String) -> void:
 	join_room_node.visible = false
 	matching_node.visible = true
+
+	matching_node.setup(hostname,password)
