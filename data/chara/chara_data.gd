7a72eb069745d20a50339d8c9927e6fec214b01f
moyashi170607 <96937973+moyashi170607@users.noreply.github.com>
2026-07-04 00:32:02 +0900

    feat:割り振った作業のためのファイルを追加

---
+++ data/chara/chara_data.gd
@@ -0,0 +1,7 @@
+class_name CharaData
+extends Resource
+
+@export var id: StringName        # 通信・セーブ用の安定ID（"attacker" 等）
+@export var display_name: String
+@export var max_hp: int
+@export var attack: int
---
+++ script/battle/chara.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+## 自分のキャラを表示する用のスプライトか
+@export var is_player:bool
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func setup(chara:) -> void:
+	pass
