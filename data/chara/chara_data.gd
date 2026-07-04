8b487925d7a0fc5395f8883eaac32c56c2ef6ddc
moyashi170607 <96937973+moyashi170607@users.noreply.github.com>
2026-07-04 17:24:07 +0900

    add:仮ファイル等の追加

--- data/chara/chara_data.gd
+++ data/chara/chara_data.gd
@@ -2,6 +2,9 @@ class_name CharaData
 extends Resource
 
 @export var id: StringName        # 通信・セーブ用の安定ID（"attacker" 等）
+@export var sprite_frames:SpriteFrames
 @export var display_name: String
 @export var max_hp: int
+@export var max_mp:int
 @export var attack: int
+@export var skills:Dictionary
