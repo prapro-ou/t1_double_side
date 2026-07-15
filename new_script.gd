6ffb4b11157849786b35c08aa63a00ccfc3de6ee
Ohtake <pq7w26fd@s.okayama-u.ac.jp>
2026-07-15 11:41:55 +0900

    残りターン数の設定

---
+++ new_script.gd
@@ -0,0 +1,10 @@
+extends Node
+
+@onready var turn_label: Label = $"Turn Label"
+
+# Called when the node enters the scene tree for the first time.
+
+#ターン表示を更新する関数
+func update_turn_display(turn) -> void:
+	turn_label.text = "残りターン"+ str(turn)
+#ターン消費したときに呼ぶ関数
