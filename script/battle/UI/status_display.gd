a609ee8f67703cc104ab41b5352a4982b1d69754
Ryusei-Katayama <pb341z61@s.okayama-u.ac.jp>
2026-07-15 11:05:44 +0900

    in the middle

--- script/battle/UI/status_display.gd
+++ script/battle/UI/status_display.gd
@@ -1,15 +1,43 @@
-extends Control
+extends Node
 
+@onready var user_name_label = $UserName
+@onready var chara_name_label = $CharaName
+@onready var hp_bar = $HP
+@onready var mp_bar = $MP
 
-## キャラ名、ユーザー名、最大HP、最大MPを設定
-func setup(username:String,chara:CharaData) -> void:
-	pass
+#ステータス表示を初期化
+func setup(username: String, chara: Dictionary) -> void:
 	
+	user_name_label.text = username
+	chara_name_label.text = chara.display_name
+	
+	hp_bar.max_value = chara.max_hp
+	mp_bar.max_value = chara.max_mp
+	
+	hp_bar.value = chara.max_hp
+	mp_bar.value = chara.max_mp
 
-## HPの値を変更
-func update_hp(hp:int) -> void:
-	pass
+#HPの現在値を変更
+func update_hp(current_hp: int) -> void:
+	hp_bar.value = current_hp
 
-## MPの値を変更
-func update_mp(mp:int) -> void:
-	pass
+#MPの現在値を変更
+func update_mp(current_mp: int) -> void:
+	mp_bar.value = current_mp
+
+
+
+func _ready() -> void:
+	
+	var dummy_chara = {
+		"display_name": "テスト勇者",
+		"max_hp": 100,
+		"max_mp": 50
+	}
+	
+	setup("Player No1", dummy_chara)
+	
+	
+	await get_tree().create_timer(2.0).timeout
+	update_hp(75)  # HPを75にする
+	update_mp(20)  # MPを20にする
