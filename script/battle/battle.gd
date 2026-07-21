7178a4e330574415a6d51ee66c9fc27300c1b431
moyashi170607 <96937973+moyashi170607@users.noreply.github.com>
2026-07-21 11:16:13 +0900

    add:ダメージ処理の仮組

--- script/battle/battle.gd
+++ script/battle/battle.gd
@@ -1,5 +1,15 @@
 extends Node2D
 
+enum Player{
+	HOST,
+	JOIN
+}
+
+var host_hp:int = 0
+var join_hp:int = 0
+
+@onready var player_chara_node:CharaBase = $CharaManager/PlayerChara
+@onready var opponent_chara_node:CharaBase = $CharaManager/OpponentChara
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,4 +18,8 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	pass
+
+## recent_hp_data = { Player.Host: "Hostの体力", Player.JOIN:"Joinの体力"}
+func play_damage_effect(recent_hp_data:Dictionary[Player,int],target:Player) -> void:
 	pass
