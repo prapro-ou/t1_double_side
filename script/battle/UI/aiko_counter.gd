048ef03ea00cfdd7033f13d5fed3d20c38330cf8
moyashi170607 <96937973+moyashi170607@users.noreply.github.com>
2026-08-21 12:29:03 +0900

    feat:あいこによるダメージの増加

--- script/battle/UI/aiko_counter.gd
+++ script/battle/UI/aiko_counter.gd
@@ -1,16 +1,13 @@
 extends Control
 
-@export var is_debug:bool = false
-
 @onready var label: Label = $Panel/Label
+@onready var rate_label:Label = $Panel/RateLabel
 
-func _ready() -> void:
-	if is_debug:
-		set_count(10)
 
-func set_count(count: int):
+func set_count(count: int,rate :float):
 	if count <= 0:
 		visible = false
 	else:
 		visible = true
-		label.text = "あいこカウント\n" + "%d" %count
+		label.text = "あいこカウント" + "%d" %count
+		rate_label.text = ("ダメージ倍率" + "%0.1f" + "倍") % rate
