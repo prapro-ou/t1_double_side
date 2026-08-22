571c7ad3e89a9ef1b9f820dc9c02afc5202bbcd0
moyashi170607 <96937973+moyashi170607@users.noreply.github.com>
2026-08-22 12:50:35 +0900

    feat:マッチングのフォント

--- script/lobby/host/matching_host.gd
+++ script/lobby/host/matching_host.gd
@@ -12,13 +12,13 @@ var peer_names:Dictionary[int,String] = {}
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	join_label_node.text = ""
-	password_label_node.text = ""
+	join_label_node.text = "対戦相手："
+	password_label_node.text = "パスワード："
 
 func setup(password:String) -> void:
 	_set_start_enabled(false)
-	password_label_node.text = password
-	join_label_node.text = WAITING_TEXT
+	password_label_node.text = "パスワード：" + password
+	join_label_node.text =  "対戦相手：" +WAITING_TEXT
 	NetworkManager.peer_joined.connect(_on_peer_joined)
 	NetworkManager.peer_left.connect(_on_peer_left)
 	# WebRTCのP2P接続が実際に確立/切断されたタイミング
@@ -30,14 +30,14 @@ func _set_start_enabled(enabled:bool) -> void:
 	start_button_node.visible = enabled
 
 func _reset_to_waiting() -> void:
-	join_label_node.text = WAITING_TEXT
+	join_label_node.text = "対戦相手：" + WAITING_TEXT
 	_set_start_enabled(false)
 
 ## シグナリングサーバー経由の参加通知。
 ## この時点ではまだWebRTCの接続が確立しておらずRPCが届かないため、開始ボタンは有効にしない
 func _on_peer_joined(id:int,username:String) -> void:
 	peer_names[id] = username
-	join_label_node.text = CONNECTING_TEXT
+	join_label_node.text = "対戦相手：" + CONNECTING_TEXT
 
 func _on_peer_left(id:int,_username:String) -> void:
 	peer_names.erase(id)
@@ -46,7 +46,7 @@ func _on_peer_left(id:int,_username:String) -> void:
 
 ## WebRTCのP2P接続が確立した。ここで初めてRPCが相手に届くようになる
 func _on_multiplayer_peer_connected(id:int) -> void:
-	join_label_node.text = peer_names.get(id, CONNECTING_TEXT)
+	join_label_node.text = "対戦相手：" + peer_names.get(id, CONNECTING_TEXT)
 	_set_start_enabled(true)
 
 func _on_multiplayer_peer_disconnected(id:int) -> void:
--- script/lobby/join/matching_join.gd
+++ script/lobby/join/matching_join.gd
@@ -5,9 +5,9 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	host_label_node.text = ""
-	password_label_node.text = ""
+	host_label_node.text = "対戦相手："
+	password_label_node.text = "パスワード："
 
 func setup(hostname:String,password:String) -> void:
-	host_label_node.text = hostname;
-	password_label_node.text = password
+	host_label_node.text = "対戦相手：" + hostname;
+	password_label_node.text = "パスワード：" + password
